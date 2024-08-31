27336ce865258122b8696756e2ef3f9cc517d54a
abusayedsohag <abusayedsohagislam25@gmail.com>
2024-08-31 02:54:09 -0400

    js array includes mathod done

---
+++ array_includes.js
@@ -0,0 +1,11 @@
+// 3. Checking Array Membership with ‘includes’
+// Instructions:
+
+// Create an array of books containing different book.
+
+// Use the includes method to check if the array contains a javascript book.
+
+// Print a message to the console indicating whether the element is present in the array or not.
+
+const book = ['Islam', 'Hadis', 'Quran'];
+console.log(book.includes('botol'));
