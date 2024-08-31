22c6f6616c9ab57aed7c88e69a41aaeb98e98b89
abusayedsohag <abusayedsohagislam25@gmail.com>
2024-08-31 02:43:51 -0400

    JS Array first task done

---
+++ declare_array.js
@@ -0,0 +1,10 @@
+/*1. Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array*/
+
+const fruits = ['Apple', 'Banana', 'Orange', 'Pape', 'kathal'];
+console.log(fruits[2]);
+fruits[1] = 'Jambura';
+console.log(fruits);
